16ff277a0cfdd2d569602e793861e954662369e5
EC2 Default User <ec2-user@ip-172-31-0-203.ap-northeast-1.compute.internal>
2023-04-11 02:16:42 +0000

    addcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/index'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
