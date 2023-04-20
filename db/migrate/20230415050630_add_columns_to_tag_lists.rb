50e10d9116ce572252c621a580856f0e5ba3c5f6
EC2 Default User <ec2-user@ip-172-31-0-203.ap-northeast-1.compute.internal>
2023-04-20 12:09:52 +0000

    addmaster.key

--- db/migrate/20230415050630_add_columns_to_tag_lists.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToTagLists < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :tag_lists, :tag, null: false, foreign_key: true
-    add_reference :tag_lists, :post, null: false, foreign_key: true
-  end
-end
