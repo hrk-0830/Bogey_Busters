a9f38b5e2ef95619079a405c0575e200ee07bf55
EC2 Default User <ec2-user@ip-172-31-0-203.ap-northeast-1.compute.internal>
2023-04-26 12:55:44 +0000

    addseed

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2023_04_23_130532) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2023_04_23_130532) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -106,12 +106,10 @@ ActiveRecord::Schema.define(version: 2023_04_23_130532) do
   end
 
   create_table "tag_lists", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "tag_id", null: false
-    t.integer "post_id", null: false
-    t.index ["post_id"], name: "index_tag_lists_on_post_id"
-    t.index ["tag_id"], name: "index_tag_lists_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -122,6 +120,4 @@ ActiveRecord::Schema.define(version: 2023_04_23_130532) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "tag_lists", "posts"
-  add_foreign_key "tag_lists", "tags"
 end
